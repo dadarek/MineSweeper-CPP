78cae869e9c378f19084af06d278cf33705c4f90
Dariusz Pasciak <dariusz@8thlight.com>
2012-06-08 15:22:58 -0500

    Adding field, columns, and rows, together into 1 struct.

--- main.cpp
+++ main.cpp
@@ -41,28 +41,30 @@ struct Field
   char spaces[10000];
   int rows;
   int columns;
-  Field(int rows, int columns)
+  Field(const char* spaces, int rows, int columns)
     : rows(rows),
       columns(columns)
-  {}
+  {
+    strcpy(this->spaces, spaces);
+  }
 };
 
-bool hasMine(const char* field, int square )
+bool hasMine(const Field& field, int square )
 {
-  return field[square] == '*';
+  return field.spaces[square] == '*';
 }
-bool isFirstColumn(int square, int columns)
+bool isFirstColumn(const Field& field, int square)
 {
-  return square % columns == 0;
+  return square % field.columns == 0;
 }
-bool isLastColumn(int square, int columns)
+bool isLastColumn(const Field& field, int square)
 {
-  return isFirstColumn(square+1, columns);
+  return isFirstColumn(field, square+1);
 }
-bool isLastRow(int square, int rows, int columns)
+bool isLastRow(const Field& field, int square)
 {
-  int totalSquares = rows * columns;
-  int sameColumnNextRow = square + columns;
+  int totalSquares = field.rows * field.columns;
+  int sameColumnNextRow = square + field.columns;
   return sameColumnNextRow >= totalSquares;
 }
 
@@ -73,152 +75,185 @@ char intToChar(int i)
   return intAsChar[0];
 }
 
-bool previousColumnHasMine(int currentSquare, const char* field, int rows, int columns)
+bool previousColumnHasMine(const Field& field, int square)
 {
   bool result = false;
-  if( !isFirstColumn(currentSquare, columns) )
+  if( !isFirstColumn(field, square) )
   {
-    result = hasMine(field, currentSquare - 1);
+    result = hasMine(field, square - 1);
   }
   return result;
 }
 
-bool nextColumnHasMine(int currentSquare, const char* field, int rows, int columns)
+bool nextColumnHasMine(const Field& field, int square)
 {
   bool result = false;
 
-  if( !isLastColumn(currentSquare, columns) )
+  if( !isLastColumn(field, square) )
   {
-    result = hasMine(field, currentSquare + 1);
+    result = hasMine(field, square + 1);
   }
   return result;
 }
 
-bool rowBelowHasMine(int currentSquare, const char* field, int rows, int columns)
+bool rowBelowHasMine(const Field& field, int square)
 {
   bool result = false;
 
-
-  if( !isLastRow(currentSquare, rows, columns) )
+  if( !isLastRow(field, square) )
   {
-    result = hasMine(field, currentSquare + columns);
+    result = hasMine(field, square + field.columns);
   }
   return result;
 }
 
-int countNeighboringMines(int currentSquare, const char* field, int rows, int columns)
+int countNeighboringMines(const Field& field, int square)
 {
   int neighboringMines = 0;
-  if( nextColumnHasMine(currentSquare, field, rows, columns) ) ++neighboringMines;
-  if( previousColumnHasMine(currentSquare, field, rows, columns) ) ++neighboringMines;
-  if( rowBelowHasMine(currentSquare, field, rows, columns) ) ++neighboringMines;
+  if( nextColumnHasMine(field, square) ) ++neighboringMines;
+  if( previousColumnHasMine(field, square) ) ++neighboringMines;
+  if( rowBelowHasMine(field, square) ) ++neighboringMines;
   return neighboringMines;
 }
 
 
-void findMines( const char* field, int rows, int columns, char* result)
+void findMines( const Field& field,  char* result)
 {
-  for(int currentColumn = 0; currentColumn < columns; currentColumn++)
+  for(int currentColumn = 0; currentColumn < field.columns; currentColumn++)
   {
-    for(int currentRow = 0; currentRow < rows; currentRow++)
+    for(int currentRow = 0; currentRow < field.rows; currentRow++)
     {
       int currentSquare = ((currentRow+1) * (currentColumn+1)) - 1;
 
-      if(field[currentSquare] == '*')
+      if(field.spaces[currentSquare] == '*')
       {
         result[currentSquare] = '*';
       }
       else
       {
-        int neighboringMines = countNeighboringMines(currentSquare, field, rows, columns);
+        int neighboringMines = countNeighboringMines(field, currentSquare);
         result[currentSquare] = intToChar( neighboringMines );
       }
     }
   }
 
-  int lastSquare = columns * rows - 1;
+  int lastSquare = field.columns * field.rows - 1;
   result[ lastSquare+1 ] = '\0';
 }
 
 void RUN_TESTS()
 {
-  char field[10000];
   char result[10000];
 
-  strcpy(field, ".");
-  findMines(field, 1, 1, result);
-  ASSERT_STREQ("0", result);
+  {
+    Field field(".", 1, 1);
+    findMines(field, result);
+    ASSERT_STREQ("0", result);
+  }
 
-  strcpy(field, "*");
-  findMines(field, 1, 1, result);
-  ASSERT_STREQ("*", result);
+  {
+    Field field("*", 1, 1);
+    findMines(field, result);
+    ASSERT_STREQ("*", result);
+  }
 
-  strcpy(field, "..");
-  findMines(field, 1, 2, result);
-  ASSERT_STREQ("00", result);
+  {
+    Field field("..", 1, 2);
+    findMines(field, result);
+    ASSERT_STREQ("00", result);
+  }
 
-  strcpy(field, "**");
-  findMines(field, 1, 2, result);
-  ASSERT_STREQ("**", result);
+  {
+    Field field("**", 1, 2);
+    findMines(field, result);
+    ASSERT_STREQ("**", result);
+  }
 
-  strcpy(field, ".*");
-  findMines(field, 1, 2, result);
-  ASSERT_STREQ("1*", result);
+  {
+    Field field(".*", 1, 2);
+    findMines(field, result);
+    ASSERT_STREQ("1*", result);
+  }
 
-  strcpy(field, "*.");
-  findMines(field, 1, 2, result);
-  ASSERT_STREQ("*1", result);
+  {
+    Field field("*.", 1, 2);
+    findMines(field, result);
+    ASSERT_STREQ("*1", result);
+  }
 
-  strcpy(field, "...");
-  findMines(field, 1, 3, result);
-  ASSERT_STREQ("000", result);
+  {
+    Field field("...", 1, 3);
+    findMines(field, result);
+    ASSERT_STREQ("000", result);
+  }
 
-  strcpy(field, "***");
-  findMines(field, 1, 3, result);
-  ASSERT_STREQ("***", result);
+  {
+    Field field("***", 1, 3);
+    findMines(field, result);
+    ASSERT_STREQ("***", result);
+  }
 
-  strcpy(field, "*..");
-  findMines(field, 1, 3, result);
-  ASSERT_STREQ("*10", result);
+  {
+    Field field("*..", 1, 3);
+    findMines(field, result);
+    ASSERT_STREQ("*10", result);
+  }
 
-  strcpy(field, "..*");
-  findMines(field, 1, 3, result);
-  ASSERT_STREQ("01*", result);
+  {
+    Field field("..*", 1, 3);
+    findMines(field, result);
+    ASSERT_STREQ("01*", result);
+  }
 
-  strcpy(field, ".*.");
-  findMines(field, 1, 3, result);
-  ASSERT_STREQ("1*1", result);
+  {
+    Field field(".*.", 1, 3);
+    findMines(field, result);
+    ASSERT_STREQ("1*1", result);
+  }
 
-  strcpy(field, "*.*");
-  findMines(field, 1, 3, result);
-  ASSERT_STREQ("*2*", result);
+  {
+    Field field("*.*", 1, 3);
+    findMines(field, result);
+    ASSERT_STREQ("*2*", result);
+  }
 
-  strcpy(field, "**.");
-  findMines(field, 1, 3, result);
-  ASSERT_STREQ("**1", result);
+  {
+    Field field("**.", 1, 3);
+    findMines(field, result);
+    ASSERT_STREQ("**1", result);
+  }
 
-  strcpy(field, ".**");
-  findMines(field, 1, 3, result);
-  ASSERT_STREQ("1**", result);
+  {
+    Field field(".**", 1, 3);
+    findMines(field, result);
+    ASSERT_STREQ("1**", result);
+  }
 
   // ROWS
 
-  strcpy(field, "..");
-  findMines(field, 2, 1, result);
-  ASSERT_STREQ("00", result);
+  {
+    Field field("..", 2, 1);
+    findMines(field, result);
+    ASSERT_STREQ("00", result);
+  }
 
-  strcpy(field, "**");
-  findMines(field, 2, 1, result);
-  ASSERT_STREQ("**", result);
+  {
+    Field field("**", 2, 1);
+    findMines(field, result);
+    ASSERT_STREQ("**", result);
+  }
 
-  strcpy(field, ".*");
-  findMines(field, 2, 1, result);
-  ASSERT_STREQ("1*", result);
+  {
+    Field field(".*", 2, 1);
+    findMines(field, result);
+    ASSERT_STREQ("1*", result);
+  }
 
-  //strcpy(field, "*.");
-  //findMines(field, 2, 1, result);
-  //ASSERT_STREQ("*1", result);
-
+  {
+    //Field field("*.", 2, 1);
+    //findMines(field, result);
+    //ASSERT_STREQ("*1", result);
+  }
 
   if(__TOTAL_FAILURES__ > 0)
     printf("\033[0;31m");
